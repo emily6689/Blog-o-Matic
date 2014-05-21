b3a5bdc0469d142f92a31d1b9211277a9518ebad
Emily Williams <ecwilliams66@gmail.com>
2014-05-21 16:40:32 -0400

    index page shows latest blog post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,15 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    if params[:id]
+      @post = Post.find(params[:id])
+    else
+      @post = Post.last
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def new
